a0a9dc29d651ab510925032e66b3d7dba6368d47
Marwan Hawari <marwan.hawari@gmail.com>
2023-04-09 10:16:29 -0700

    wip resizer works, but content is cut off

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,3 +1,4 @@
+//@ts-nocheck
 const APP_URL = import.meta.env.VITE_APP_URL;
 
 async function main() {
@@ -6,6 +7,123 @@ async function main() {
     reactRoot.src = APP_URL;
     reactRoot.id = "leetrooms-iframe";
     reactRoot.allow = "clipboard-read; clipboard-write";
+    reactRoot.style.height = "100%";
+    reactRoot.style.width = "calc(100% - 10px)"; // Add this line to make the iframe take the full width of the wrapper, minus the handlebar width
+
+    const wrapper = document.createElement("div");
+    wrapper.style.position = "relative";
+    wrapper.style.display = "inline-block";
+    wrapper.style.overflow = "hidden";
+    wrapper.appendChild(reactRoot);
+
+    chrome.storage.local.get("leetroomsWidth", (result) => {
+        const leetroomsWidth = result.leetroomsWidth ?? "525";
+        reactRoot.style.width = `${leetroomsWidth}px`;
+        wrapper.style.width = `${leetroomsWidth}px`; // Set the initial width of the wrapper element
+    });
+
+    const handlebar = document.createElement("div");
+    handlebar.style.position = "absolute";
+    handlebar.style.left = "-10px"; // Adjust the horizontal position
+    handlebar.style.width = "20px"; // Increase the width
+    handlebar.style.top = "0";
+    handlebar.style.height = "100%";
+    handlebar.style.cursor = "col-resize";
+    handlebar.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+    handlebar.style.zIndex = "10";
+    handlebar.style.borderRadius = "4px";
+    wrapper.appendChild(handlebar);
+
+    let isResizing = false;
+    let initialMousePosition = 0;
+
+    const overlay = document.createElement("div");
+    overlay.style.position = "absolute";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.display = "none";
+    wrapper.appendChild(overlay);
+
+    const startResizing = (event) => {
+        isResizing = true;
+        initialMousePosition = event.clientX;
+        overlay.style.display = "block"; // Show the overlay
+    };
+
+    handlebar.style.userSelect = "none"; // Add this line to disable text selection on the handlebar
+
+    handlebar.addEventListener("mousedown", startResizing);
+    handlebar.addEventListener("dragstart", (event) => event.preventDefault()); // Prevent the default drag behavior
+
+    const stopResizing = () => {
+        isResizing = false;
+        overlay.style.display = "none"; // Hide the overlay
+    };
+
+    handlebar.addEventListener("mousedown", startResizing);
+
+    const throttle = (func, limit) => {
+        let inThrottle;
+        return (...args) => {
+            if (!inThrottle) {
+                func.apply(null, args);
+                inThrottle = true;
+                setTimeout(() => (inThrottle = false), limit);
+            }
+        };
+    };
+
+    const MIN_WIDTH = 375; // Minimum width constraint
+    const MAX_WIDTH = 800; // Maximum width constraint
+
+    const updateWidth = (event) => {
+        if (!isResizing) return;
+        const deltaX = initialMousePosition - event.clientX;
+        initialMousePosition = event.clientX;
+        const currentWidth = parseInt(reactRoot.style.width);
+        let newWidth = currentWidth + deltaX;
+
+        if (newWidth < MIN_WIDTH) {
+            newWidth = MIN_WIDTH;
+            if (
+                deltaX < 0 &&
+                event.clientX > handlebar.getBoundingClientRect().right
+            ) {
+                initialMousePosition = event.clientX;
+            }
+        } else if (newWidth > MAX_WIDTH) {
+            newWidth = MAX_WIDTH;
+            if (
+                deltaX > 0 &&
+                event.clientX < handlebar.getBoundingClientRect().left
+            ) {
+                initialMousePosition = event.clientX;
+            }
+        } else {
+            if (
+                deltaX < 0 &&
+                event.clientX > handlebar.getBoundingClientRect().right
+            ) {
+                newWidth = Math.max(MIN_WIDTH, Math.min(newWidth, MAX_WIDTH));
+            } else if (
+                deltaX > 0 &&
+                event.clientX < handlebar.getBoundingClientRect().left
+            ) {
+                newWidth = Math.max(MIN_WIDTH, Math.min(newWidth, MAX_WIDTH));
+            } else {
+                return;
+            }
+        }
+
+        reactRoot.style.width = `${newWidth}px`;
+        wrapper.style.width = `${newWidth}px`;
+        chrome.storage.local.set({ leetroomsWidth: newWidth });
+    };
+    window.addEventListener("mousemove", throttle(updateWidth, 16));
+    window.addEventListener("mouseup", stopResizing);
+
     chrome.storage.local.get("leetroomsToggleState", (result) => {
         const toggleState = result.leetroomsToggleState ?? true;
         if (toggleState) {
@@ -20,7 +138,8 @@ async function main() {
     });
 
     const mainContentContainer = await waitForElement(["#qd-content"]);
-    mainContentContainer.insertAdjacentElement("afterend", reactRoot);
+    // mainContentContainer.insertAdjacentElement("afterend", reactRoot);
+    mainContentContainer.insertAdjacentElement("afterend", wrapper);
 
     const submissionButtonSelectors = [
         "#qd-content > div > div> div:nth-child(3) > div > div > div > div > div > div:nth-last-child(1) > button:nth-last-child(1)",
