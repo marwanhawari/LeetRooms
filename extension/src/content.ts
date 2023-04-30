20db547c0396002c49edb987daada071ff94fecf
Marwan Hawari <marwan.hawari@gmail.com>
2023-04-29 22:25:15 -0700

    working

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -7,35 +7,20 @@ async function main() {
     reactRoot.src = APP_URL;
     reactRoot.id = "leetrooms-iframe";
     reactRoot.allow = "clipboard-read; clipboard-write";
-    reactRoot.style.height = "100%";
-    reactRoot.style.width = "calc(100% - 10px)"; // Add this line to make the iframe take the full width of the wrapper, minus the handlebar width
 
     const wrapper = document.createElement("div");
-    wrapper.style.position = "relative";
-    wrapper.style.display = "inline-block";
     wrapper.style.overflow = "hidden";
-    wrapper.appendChild(reactRoot);
-
-    chrome.storage.local.get("leetroomsWidth", (result) => {
-        const leetroomsWidth = result.leetroomsWidth ?? "525";
-        reactRoot.style.width = `${leetroomsWidth}px`;
-        wrapper.style.width = `${leetroomsWidth}px`; // Set the initial width of the wrapper element
-    });
+    wrapper.style.display = "flex";
+    wrapper.style.position = "relative";
+    wrapper.style.flexDirection = "row";
+    wrapper.style.height = "100%";
 
     const handlebar = document.createElement("div");
-    handlebar.style.position = "absolute";
-    handlebar.style.left = "-10px"; // Adjust the horizontal position
-    handlebar.style.width = "20px"; // Increase the width
-    handlebar.style.top = "0";
-    handlebar.style.height = "100%";
+    handlebar.style.minWidth = "10px";
     handlebar.style.cursor = "col-resize";
     handlebar.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
     handlebar.style.zIndex = "10";
-    handlebar.style.borderRadius = "4px";
-    wrapper.appendChild(handlebar);
-
-    let isResizing = false;
-    let initialMousePosition = 0;
+    handlebar.style.userSelect = "none"; // Add this line to disable text selection on the handlebar
 
     const overlay = document.createElement("div");
     overlay.style.position = "absolute";
@@ -44,15 +29,20 @@ async function main() {
     overlay.style.width = "100%";
     overlay.style.height = "100%";
     overlay.style.display = "none";
+
+    wrapper.appendChild(handlebar);
+    wrapper.appendChild(reactRoot);
     wrapper.appendChild(overlay);
+
+    let isResizing = false;
+    let initialMousePosition = 0;
 
     const startResizing = (event) => {
         isResizing = true;
         initialMousePosition = event.clientX;
         overlay.style.display = "block"; // Show the overlay
+        // overlay.style.backgroundColor = "red";
     };
-
-    handlebar.style.userSelect = "none"; // Add this line to disable text selection on the handlebar
 
     handlebar.addEventListener("mousedown", startResizing);
     handlebar.addEventListener("dragstart", (event) => event.preventDefault()); // Prevent the default drag behavior
@@ -61,8 +51,6 @@ async function main() {
         isResizing = false;
         overlay.style.display = "none"; // Hide the overlay
     };
-
-    handlebar.addEventListener("mousedown", startResizing);
 
     const throttle = (func, limit) => {
         let inThrottle;
@@ -75,7 +63,7 @@ async function main() {
         };
     };
 
-    const MIN_WIDTH = 375; // Minimum width constraint
+    const MIN_WIDTH = 400; // Minimum width constraint
     const MAX_WIDTH = 800; // Maximum width constraint
 
     const updateWidth = (event) => {
@@ -118,11 +106,14 @@ async function main() {
         }
 
         reactRoot.style.width = `${newWidth}px`;
-        wrapper.style.width = `${newWidth}px`;
         chrome.storage.local.set({ leetroomsWidth: newWidth });
     };
     window.addEventListener("mousemove", throttle(updateWidth, 16));
     window.addEventListener("mouseup", stopResizing);
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     chrome.storage.local.get("leetroomsToggleState", (result) => {
         const toggleState = result.leetroomsToggleState ?? true;
