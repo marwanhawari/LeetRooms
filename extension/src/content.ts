a68e449a9fa11e17305fab40afc5fddb4a51e888
Marwan Hawari <59078997+marwanhawari@users.noreply.github.com>
2023-05-21 01:57:13 -0700

    feat: drag to resize in the UI (#36)

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,5 +1,15 @@
 const APP_URL = import.meta.env.VITE_APP_URL;
 
+const dragHandlebarSVG = `<svg class="handlebar-svg" id="drag-handlebar-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 14" width="2" height="14">
+<circle r="1" transform="matrix(4.37114e-08 -1 -1 -4.37114e-08 1 1)"></circle>
+<circle r="1" transform="matrix(4.37114e-08 -1 -1 -4.37114e-08 1 7)"></circle>
+<circle r="1" transform="matrix(4.37114e-08 -1 -1 -4.37114e-08 1 13)"></circle>
+    </svg>`;
+
+const openHandlebarSVG = `<svg class="handlebar-svg" id="open-handlebar-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+    <path fill-rule="evenodd" d="M7.913 19.071l7.057-7.078-7.057-7.064a1 1 0 011.414-1.414l7.764 7.77a1 1 0 010 1.415l-7.764 7.785a1 1 0 01-1.414-1.414z" clip-rule="evenodd"></path>
+    </svg>`;
+
 async function main() {
     let previousSubmissionId = "";
     const reactRoot = document.createElement("iframe");
@@ -7,12 +17,147 @@ async function main() {
     reactRoot.src = APP_URL;
     reactRoot.id = "leetrooms-iframe";
     reactRoot.allow = "clipboard-read; clipboard-write";
+
+    const handlebar = document.createElement("div");
+    handlebar.id = "leetrooms-handlebar";
+    handlebar.style.minWidth = "8px";
+    handlebar.style.userSelect = "none"; // This line disables text selection on the handlebar
+    handlebar.style.position = "relative";
+    handlebar.style.left = "-4px";
+
+    const overlay = document.createElement("div");
+    overlay.style.position = "absolute";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.display = "none";
+
+    let isResizing = false;
+    let initialMousePosition = 0;
+    let isOpen = true;
+
+    function startResizing(event: MouseEvent) {
+        isResizing = true;
+        initialMousePosition = event.clientX;
+        overlay.style.display = "block";
+    }
+
+    handlebar.addEventListener("mousedown", (event) => {
+        if (!isOpen) {
+            setToggleState(true);
+            return;
+        }
+        startResizing(event);
+    });
+    handlebar.addEventListener("dragstart", (event) => event.preventDefault()); // Prevent the default drag behavior
+
+    function setToggleState(toggleState: boolean) {
+        if (toggleState) {
+            reactRoot.style.display = "block";
+            handlebar.innerHTML = dragHandlebarSVG;
+            handlebar.style.cursor = "col-resize";
+            chrome.storage.local.set({ leetroomsToggleState: true });
+            isOpen = true;
+            handlebar.style.zIndex = "10";
+        } else {
+            reactRoot.style.display = "none";
+            handlebar.innerHTML = openHandlebarSVG;
+            handlebar.style.cursor = "pointer";
+            chrome.storage.local.set({ leetroomsToggleState: false });
+            isOpen = false;
+            handlebar.style.zIndex = "0";
+        }
+    }
+
+    handlebar.addEventListener("dblclick", () => {
+        if (isOpen) {
+            setToggleState(false);
+        }
+    });
+
+    function stopResizing() {
+        isResizing = false;
+        overlay.style.display = "none"; // Hide the overlay
+    }
+
+    function throttle(func: any, limit: number) {
+        let inThrottle: boolean;
+        return (...args: any) => {
+            if (!inThrottle) {
+                func.apply(null, args);
+                inThrottle = true;
+                setTimeout(() => (inThrottle = false), limit);
+            }
+        };
+    }
+
+    const MIN_WIDTH = 400;
+    const MAX_WIDTH = 800;
+
+    function updateWidth(event: MouseEvent) {
+        if (!isResizing) return;
+        const deltaX = initialMousePosition - event.clientX;
+        initialMousePosition = event.clientX;
+        const currentWidth = parseInt(reactRoot.style.width);
+        let newWidth = currentWidth + deltaX;
+        if (
+            isOpen &&
+            initialMousePosition - window.innerWidth - MIN_WIDTH > -500
+        ) {
+            setToggleState(false);
+            return;
+        } else if (
+            !isOpen &&
+            initialMousePosition - window.innerWidth - MIN_WIDTH < -500
+        ) {
+            setToggleState(true);
+            return;
+        }
+        if (newWidth < MIN_WIDTH) {
+            newWidth = MIN_WIDTH;
+            if (
+                deltaX < 0 &&
+                event.clientX > handlebar.getBoundingClientRect().right
+            ) {
+                initialMousePosition = event.clientX;
+            }
+        } else if (newWidth > MAX_WIDTH) {
+            newWidth = MAX_WIDTH;
+            if (
+                deltaX > 0 &&
+                event.clientX < handlebar.getBoundingClientRect().left
+            ) {
+                initialMousePosition = event.clientX;
+            }
+        } else {
+            if (
+                deltaX < 0 &&
+                event.clientX > handlebar.getBoundingClientRect().right
+            ) {
+                newWidth = Math.max(MIN_WIDTH, Math.min(newWidth, MAX_WIDTH));
+            } else if (
+                deltaX > 0 &&
+                event.clientX < handlebar.getBoundingClientRect().left
+            ) {
+                newWidth = Math.max(MIN_WIDTH, Math.min(newWidth, MAX_WIDTH));
+            } else {
+                return;
+            }
+        }
+
+        reactRoot.style.width = `${newWidth}px`;
+        chrome.storage.local.set({ leetroomsWidth: newWidth });
+    }
+    window.addEventListener("mousemove", throttle(updateWidth, 16));
+    window.addEventListener("mouseup", stopResizing);
+
     chrome.storage.local.get("leetroomsToggleState", (result) => {
         const toggleState = result.leetroomsToggleState ?? true;
         if (toggleState) {
-            reactRoot.style.display = "block";
+            setToggleState(true);
         } else {
-            reactRoot.style.display = "none";
+            setToggleState(false);
         }
     });
     chrome.storage.local.get("leetroomsWidth", (result) => {
@@ -21,7 +166,9 @@ async function main() {
     });
 
     const mainContentContainer = await waitForElement(["#qd-content"]);
+    mainContentContainer.insertAdjacentElement("afterend", overlay);
     mainContentContainer.insertAdjacentElement("afterend", reactRoot);
+    mainContentContainer.insertAdjacentElement("afterend", handlebar);
 
     let submissionButtonTimer: number;
     async function handleClickSubmitCodeButton(submissionId: string) {
