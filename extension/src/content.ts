7fbb4979a47dbfe87941c4e77f3ff2ae1dcfd336
Marwan Hawari <marwan.hawari@gmail.com>
2023-05-20 22:18:25 -0700

    fix margin bottom of < symbol

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -7,7 +7,7 @@ const dragHandlebarSVG = `<svg id="drag-handlebar-svg" xmlns="http://www.w3.org/
     <circle r="1" transform="matrix(4.37114e-08 -1 -1 -4.37114e-08 1 13)"></circle>
     </svg>`;
 
-const openHandlebarSVG = `<svg id="open-handlebar-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="white">
+const openHandlebarSVG = `<svg id="open-handlebar-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="white" style="margin-bottom: 48px;">
     <path fill-rule="evenodd" d="M7.913 19.071l7.057-7.078-7.057-7.064a1 1 0 011.414-1.414l7.764 7.77a1 1 0 010 1.415l-7.764 7.785a1 1 0 01-1.414-1.414z" clip-rule="evenodd"></path>
     </svg>`;
 
