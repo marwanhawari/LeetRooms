4b7ff099a524f3a5743d11fdfef796845d8a0bec
Marwan Hawari <59078997+marwanhawari@users.noreply.github.com>
2024-04-14 01:56:18 -0700

    feat: add toggle to completely hide panel, show version in extension popup (#63)
    * re-add 'show leetrooms panel' button
    
    * clean up styles in popup

--- extension/src/public/popup.js
+++ extension/src/public/popup.js
@@ -1,7 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("./manifest.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const version = data.version;
+            document.querySelector(
+                "#version"
+            ).textContent = `Version ${version}`;
+        })
+        .catch((error) => console.error("Error loading the version:", error));
+});
+
 const darkModeButton = document.querySelector("#toggle-leetrooms-darkmode");
 darkModeButton.addEventListener("click", () => {
     const darkModeState = darkModeButton.checked;
     chrome.storage.local.set({ leetroomsDarkMode: darkModeState });
+});
+
+const showHideButton = document.querySelector("#show-hide-leetrooms");
+showHideButton.addEventListener("click", () => {
+    const shouldShowPanel = showHideButton.checked;
+    chrome.storage.local.set({ shouldShowPanel: shouldShowPanel });
+    if (shouldShowPanel) {
+        darkModeButton.parentElement.parentElement.style.display = "flex";
+    } else {
+        darkModeButton.parentElement.parentElement.style.display = "none";
+    }
 });
 
 const instructionsContainer = document.querySelector("#leetrooms-instructions");
@@ -9,15 +32,26 @@ const settingsContainer = document.querySelector("#leetrooms-settings");
 chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
     const currentUrl = tabs[0].url;
     if (currentUrl.includes("https://leetcode.com")) {
-        settingsContainer.style.display = "block";
+        settingsContainer.style.display = "flex";
         instructionsContainer.style.display = "none";
+        chrome.storage.local.get("shouldShowPanel", (result) => {
+            const shouldShowPanel = result.shouldShowPanel ?? true;
+            showHideButton.checked = shouldShowPanel;
+            if (shouldShowPanel) {
+                darkModeButton.parentElement.parentElement.style.display =
+                    "flex";
+            } else {
+                darkModeButton.parentElement.parentElement.style.display =
+                    "none";
+            }
+        });
         chrome.storage.local.get("leetroomsDarkMode", (result) => {
             const darkModeState = result.leetroomsDarkMode ?? true;
             darkModeButton.checked = darkModeState;
         });
     } else {
         settingsContainer.style.display = "none";
-        instructionsContainer.style.display = "block";
+        instructionsContainer.style.display = "flex";
     }
 });
 
